05aafbef69eb85e2ddaac74c5ddfec708221716d
Ed Wied <wied.ed@gmail.com>
2022-01-30 11:23:19 -0600

    added expanding text on homepage

--- public/js/homepageScripts.js
+++ public/js/homepageScripts.js
@@ -7,9 +7,21 @@
 
 document.addEventListener("DOMContentLoaded", async function(event) {
   //wire up start buttoon
-  document.getElementById("gameStart").addEventListener("click", playGame);
+  //document.getElementById("gameStart").addEventListener("click", playGame);
 });
 
 function playGame() {
   window.location = "/play";
+}
+
+function showHideDescriptionText() {
+  let currentState = document.getElementById('addedDescription').style.display;
+  if (currentState === "none") {
+    document.getElementById('addedDescription').style.display = "block";
+    document.getElementById('readMoreLink').text = "Hide";
+  } else {
+    document.getElementById('addedDescription').style.display = "none";
+    document.getElementById('readMoreLink').text = "Read more";
+  }
+  return false;
 }
